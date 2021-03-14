043fa9252a4c1a41b123c0b20595b2e9af9282e3
samuelp88 <41595779+samuelp88@users.noreply.github.com>
2021-03-14 02:45:51 -0300

    Galinhas e tal

---
+++ canvas.js
@@ -0,0 +1,67 @@
+
+
+
+
+
+function start() {
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    
+    class Galinha {
+        constructor() {
+            this.position = {
+                x: 0,
+                y: 0,
+            };
+    
+            this.size = {
+                width: 75,
+                height: 75,
+            };
+    
+            this.sprite = new Image();
+            this.sprite.src = "./coisas/galinhafoda.png";
+    
+            this.speed = 2;
+
+            this.render = function(){
+                ctx.drawImage(this.sprite, this.position.x, this.position.y, this.size.width, this.size.height);
+            }
+        }
+    }
+
+    var galinhafoda = new Galinha();
+    galinhafoda.position.y = 50;
+    galinhafoda.position.x = 0;
+
+
+
+
+    update();
+
+
+    function update() {
+        
+        galinhafoda.render();
+
+        
+    
+        galinhafoda.position.x += galinhafoda.speed;
+        galinhafoda.size.height += galinhafoda.speed;
+        galinhafoda.size.width += galinhafoda.speed;
+        if(galinhafoda.size.width > 1200) {
+            galinhafoda.size.width = 75;
+            galinhafoda.size.height = 75;
+            galinhafoda.position.y = 50;
+            galinhafoda.position.x = -75;
+            galinhafoda.speed += 0.75;
+        }
+        
+        setTimeout(() => {
+            update()
+        }, 1)
+        
+    }
+
+
+}
